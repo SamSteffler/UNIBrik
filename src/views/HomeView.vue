46804715839d4c0d1796005f4867320c93b62eff
ArthurLeitaoEtges <arthurleitaoetges@hotmail.com>
2025-09-24 15:21:40 -0300

    mudança das configurações da home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Página Inicial</h1>
+    <h1>Testche</h1>
     <p>Esta é a página principal do marketplace. Pode ser acessada por qualquer pessoa, logada ou não.</p>
     <p>Aqui você pode adicionar seções como "Categorias", "Visto recentemente" e "Anúncios em destaque", conforme o mockup.</p>
   </div>
