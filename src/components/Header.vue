a79757f35c700b3b5d16e5f53c9a50285c9e7a93
TiagoSteffler <tiago.steffler@acad.ufsm.br>
2025-09-24 15:52:18 -0300

    Adição de menu lateral e novo estilo de cabeçalho de página

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,17 @@
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import authService from '../services/authService';
+import SideMenu from './SideMenu.vue'; // 1. Importe o novo componente
 
 const router = useRouter();
 const route = useRoute();
 
-// Computa se a barra de pesquisa deve ser exibida
+// 2. Crie um estado para controlar a visibilidade do menu
+const isMenuOpen = ref(false);
+
 const showSearchBar = computed(() => route.path !== '/profile');
 
-// Redireciona para a home ao clicar no logo
 const goToHome = () => {
   router.push('/');
 }
@@ -21,16 +23,28 @@ const goToHome = () => {
 
     <div v-if="showSearchBar" class="search-bar">
       <input type="text" placeholder="TV 50 polegadas..." class="search-input" />
+      <button class="search-button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+        </svg>
+      </button>
     </div>
 
     <div class="user-area">
       <RouterLink v-if="!authService.userState.isLoggedIn" to="/login" class="profile-button">
         Login ou Cadastre-se
       </RouterLink>
-      
       <RouterLink v-else to="/profile" class="profile-button">
         Meu Perfil
       </RouterLink>
+      
+      <button class="menu-toggle" @click="isMenuOpen = true">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+      </button>
     </div>
   </header>
+
+  <SideMenu :is-open="isMenuOpen" @close="isMenuOpen = false" />
 </template>
