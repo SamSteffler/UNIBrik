de1d011c7f0962a4ae3dc2a67fbd8fcae5a6c254
SamSteffler <samuel.steffler@acad.ufsm.br>
2025-09-30 11:51:18 -0300

    feat: update search page title to display current search query

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -8,7 +8,7 @@ const searchQuery = ref(route.query.q || '');
 </script>
 <template>
   <div>
-    <h1>Página de Pesquisa de Produtos</h1>
+    <h1>Pesquisa - {{ searchQuery }}</h1>
     <p>Aqui seriam exibidos os resultados da busca, com filtros na lateral.</p>
     <p>Esta página também é pública e pode ser acessada sem login.</p>
   </div>
