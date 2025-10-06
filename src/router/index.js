9e4d938be2c9f197d8998c3b1e4d8dd917d7ed97
TiagoSteffler <tiago.steffler@acad.ufsm.br>
2025-10-06 10:39:20 -0300

    Deletar aplicativo antigo e corrigir rota

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,8 @@
-//import { createRouter, createWebHistory } from 'vue-router'
-//import HomeView from '../views/HomeView.vue'
-//
-//const router = createRouter({
-//  history: createWebHistory(import.meta.env.BASE_URL),
-//  routes: [
-//    {
-//      path: '/',
-//      name: 'home',
-//      component: HomeView,
-//    },
-//    {
-//      path: '/about',
-//      name: 'about',
-//      // route level code-splitting
-//      // this generates a separate chunk (About.[hash].js) for this route
-//      // which is lazy-loaded when the route is visited.
-//      component: () => import('../views/AboutView.vue'),
-//    },
-//  ],
-//})
-//
-//export default router
-//
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SearchView from '../views/SearchView.vue'
 import ProfileView from '../views/ProfileView.vue'
-import LoginView from '../views/LoginView.vue' // 1. Importe a nova view
+import LoginView from '../views/LoginView.vue'
 import { userState } from '../services/authService';
 
 const router = createRouter({
@@ -72,3 +48,5 @@ router.beforeEach((to, from, next) => {
 });
 
 export default router
+
+
